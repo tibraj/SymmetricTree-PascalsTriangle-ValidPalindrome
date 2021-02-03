95497e82bb570f918af96bb65f782a16b9586ba0
tibraj <tibraj94@gmail.com>
2021-02-03 14:30:02 -0500

    symmetric tree

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 //Symmetric Tree
 
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val);
+    this.left = (val === undefined ? null : left);
+    this.right = (val === undefined ? null : right);
+}
 
-
+const isSymmetric = (root) => isSameTree(root, root);
+cons isSameTree = (p, q) => !p || !q ? p === q : p.val === q.val && isSameTree(p.right, q.left) && isSameTree(p.left, q.right);
 
 //Pascal's Triangle
 
