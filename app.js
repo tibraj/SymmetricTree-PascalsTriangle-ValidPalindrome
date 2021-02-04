57365048d3895a45e57543a1611d2912a1470737
tibraj <tibraj94@gmail.com>
2021-02-04 13:38:50 -0500

    pascal's triangle

--- app.js
+++ app.js
@@ -11,7 +11,19 @@ cons isSameTree = (p, q) => !p || !q ? p === q : p.val === q.val && isSameTree(p
 
 //Pascal's Triangle
 
-
+function pascalTriangle(numRows) {
+    if(numRows === 0) return [];
+    let arr = [[1]];
+    for(let i = 1; i < numRows; i++) {
+        arr[i] = [];
+        for(let j = 0; j < i + 1; j++) {
+            const left = arr[i - 1][j - 1] || 0;
+            const right = arr[i - 1][j] || 0;
+            arr[i].push(left + right);
+        }
+    }
+    return arr;
+}
 
 
 //Valid Palindrome
